13531c6bf4d0ed08f38fee8f86624faa6366d8fa
Triumphator <86240633+Triumphator@users.noreply.github.com>
2021-07-04 14:27:27 +0600

    Add files via upload

--- mySQL movies.py
+++ mySQL movies.py
@@ -13,7 +13,7 @@ mycursor = mydb.cursor()
 def addMovie(year, movie_title, budget, director, box_office):
     global mydb
     mycursor = mydb.cursor()
-    sql = "INSERT INTO users (year, movie_title, budget, director, box_office) VALUES (%s, %s, %s, %s, %s)"
+    sql = "INSERT INTO movies (year, movie_title, budget, director, box_office) VALUES (%s, %s, %s, %s, %s)"
     values = (year, movie_title, budget, director, box_office)
     mycursor.execute(sql, values)
     mydb.commit()
@@ -44,6 +44,13 @@ def updateMovie(year, movie_title, budget, director, box_office):
     mycursor.execute(sql, values)
     mydb.commit()
 
+def onlineShop(movie_title):
+    global mydb
+    mycursor = mydb.cursor()
+    sql = "SELECT movies.Movie_title, shops.ShopName FROM movies INNER JOIN shops ON movies.Movie_title = shops.Movie_title; " \
+          "WHERE Movie_title = " + str(movie_title)
+    mycursor.execute(sql)
+    mydb.commit()
 
 while True:
     print("PRESS 1 TO ADD MOVIE")
@@ -52,55 +59,64 @@ while True:
     print("PRESS 4 TO UPDATE MOVIE")
     print("PRESS 0 TO EXIT")
 
-    choice = input()
+    choice = int(input())
+    if 0 <= choice <= 5:
+        if choice == 1:
+            print("INSERT YEAR: ")
+            year = input()
+            print("INSERT MOVIE TITLE: ")
+            movie_title = input()
+            print("INSERT BUDGET: ")
+            budget = input()
+            print("INSERT DIRECTOR: ")
+            director = input()
+            print("INSERT BOX OFFICE: ")
+            box_office = input()
 
-    if choice == "1":
-        print("INSERT YEAR: ")
-        year = input()
-        print("INSERT MOVIE TITLE: ")
-        movie_title = input()
-        print("INSERT BUDGET: ")
-        budget = input()
-        print("INSERT DIRECTOR: ")
-        director = input()
-        print("INSERT BOX OFFICE: ")
-        box_office = input()
+            addMovie(year, movie_title, budget, director, box_office)
 
-        addMovie(year, movie_title, budget, director, box_office)
+        elif choice == 2:
+            movies = getMovies()
+            for movie in movies:
+                print(movie)
 
-    elif choice == "2":
-        movies = getMovies()
-        for movie in movies:
-            print(movie)
+        elif choice == 3:
+            movies = getMovies()
+            for movie in movies:
+                print(movie)
 
-    elif choice == "3":
-        movies = getMovies()
-        for movie in movies:
-            print(movie)
+            print("CHOOSE MOVIE TITLE TO DELETE:")
+            movie_to_delete = input()
+            confirmation = input("Are you sure?\n Press Y to confirm and N to reject\n")
+            if confirmation == "y":
+                deleteMovie(movie_to_delete)
 
-        print("CHOOSE MOVIE TITLE TO DELETE:")
-        movie_to_delete = input()
-        deleteMovie(movie_to_delete)
+        elif choice == 4:
+            movies = getMovies()
+            for movie in movies:
+                print(movie)
 
-    elif choice == "4":
-        movies = getMovies()
-        for movie in movies:
-            print(movie)
+            print("CHOOSE MOVIE TITLE TO UPDATE:")
+            movie_title = input()
 
-        print("CHOOSE MOVIE TITLE TO UPDATE:")
-        movie_title = input()
+            print("INSERT NEW YEAR OF ORIGIN: ")
+            year = input()
+            print("INSERT NEW MOVIE TITLE: ")
+            movietitle = input()
+            print("INSERT NEW BUDGET: ")
+            budget = input()
+            print("INSERT NEW DIRECTOR: ")
+            director = input()
+            print("INSERT NEW BOX OFFICE: ")
+            box_office = input()
+            updateMovie(year, movietitle, budget, director, box_office)
 
-        print("INSERT NEW YEAR OF ORIGIN: ")
-        year = input()
-        print("INSERT NEW MOVIE TITLE: ")
-        movietitle = input()
-        print("INSERT NEW BUDGET: ")
-        budget = input()
-        print("INSERT NEW DIRECTOR: ")
-        director = input()
-        print("INSERT NEW BOX OFFICE: ")
-        box_office = input()
-        updateMovie(year, movietitle, budget, director, box_office)
+        elif choice == 5:
+            movies = getMovies()
+            print("Enter movie title to check"
+                  "if it is available for purchase")
+            movie_title = input()
+            onlineShop(movie_title)
 
-    elif choice == "0":
-        break
+        elif choice == 0:
+            break
